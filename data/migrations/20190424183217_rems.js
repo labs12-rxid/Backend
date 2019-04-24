380ce53736a3861a822b45c6de93185458b0b6d7
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-24 19:06:24 -0400

    Wrote migration for rems table.

---
+++ data/migrations/20190424183217_rems.js
@@ -0,0 +1,30 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('rems', table => {
+    table.increments();
+    table
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    table
+      .integer('med_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('meds')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    table
+      .string('rem_type')
+      .notNullable()
+      .defaultTo('admin');
+    table.string('rem_notes', 16);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('rems');
+};
