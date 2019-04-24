b37ef87148de960288553ae3133c71a89cf5b556
workingjubilee <workingjubilee@gmail.com>
2019-04-24 14:02:06 -0700

    Add Morgan logging per discussion, currently in verbose mode

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const morgan = require("morgan");
 // const usersRouter = require("../routers/userRouter.js");
 
 const server = express();
@@ -8,6 +9,7 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(morgan('combined'));
 
 // server.use("/api/users", usersRouter);
 
