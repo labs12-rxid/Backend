02a143a430160767b23b46173d217fb6915eb4a7
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-25 17:48:45 -0400

    Reformatted db connections for other fields in knexfile besides just development.

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,14 @@
 require("dotenv").config();
 
-const testDatabase = process.env.DATABASE_TEST;
-const productionDatabase = process.env.DATABASE_URL;
-
 module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DEV_HOSTNAME || "localhost",
-      database: process.env.DEV_DATABASE || "rxid",
-      user: process.env.DEV_USER || "admin",
-      password: process.env.DEV_PASSWORD || "pass"
+      host: process.env.DB_DEV_HOST || "localhost",
+      database: process.env.DB_DEV_DATABASE || "rxid",
+      user: process.env.DB_DEV_USER || "admin",
+      password: process.env.DB_DEV_PASSWORD || "pass",
+      port: process.env.DB_DEV_PORT || "5432"
     },
     pool: {
       min: 2,
@@ -27,7 +25,13 @@ module.exports = {
 
   testing: {
     client: "pg",
-    connection: testDatabase,
+    connection: {
+      host: process.env.DB_TEST_HOST || "localhost",
+      database: process.env.DB_TEST_DATABASE || "rxid",
+      user: process.env.DB_TEST_USER || "admin",
+      password: process.env.DB_TEST_PASSWORD || "pass",
+      port: process.env.DB_TEST_PORT || "5432"
+    },
     pool: {
       min: 2,
       max: 10
@@ -43,7 +47,13 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: productionDatabase,
+    connection: {
+      host: process.env.DB_PROD_HOST || "localhost",
+      database: process.env.DB_PROD_DATABASE || "rxid",
+      user: process.env.DB_PROD_USER || "admin",
+      password: process.env.DB_PROD_PASSWORD || "pass",
+      port: process.env.DB_PROD_PORT || "5432"
+    },
     pool: {
       min: 2,
       max: 10
