7d0ff21dd8b7380546ce61ec9a149c5e22bd3605
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-25 17:33:36 -0400

    Commented out faker fake user generator function and for loop. Imported the created dummyData and used that in seed file

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,26 +1,27 @@
 const fs = require("fs");
 const faker = require("faker");
 const bcrypt = require("bcryptjs");
+const fakeUsers = require("../dummyData/fakeUsers.json")["users"];
 
-const createFakeUser = () => ({
-  email: faker.internet.email(),
-  first_name: faker.name.firstName(),
-  last_name: faker.name.lastName(),
-  username: faker.internet.userName(),
-  password: bcrypt.hashSync(faker.internet.password(), 8)
-});
+// const createFakeUser = () => ({
+//   email: faker.internet.email(),
+//   first_name: faker.name.firstName(),
+//   last_name: faker.name.lastName(),
+//   username: faker.internet.userName(),
+//   password: bcrypt.hashSync(faker.internet.password(), 8)
+// });
 
 exports.seed = function(knex, Promise) {
-  const fakeUsers = [];
+  // const fakeUsers = [];
 
-  for (let i = 0; i < 500; i++) {
-    fakeUsers.push(createFakeUser());
-  }
+  // for (let i = 0; i < 500; i++) {
+  //   fakeUsers.push(createFakeUser());
+  // }
 
-  fs.writeFileSync(
-    "./data/dummyData/fakeUsers.json",
-    JSON.stringify({ users: fakeUsers })
-  );
+  // fs.writeFileSync(
+  //   "./data/dummyData/fakeUsers.json",
+  //   JSON.stringify({ users: fakeUsers })
+  // );
 
   return knex("users").insert(fakeUsers);
 };
