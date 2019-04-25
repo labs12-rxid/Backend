cb1fdb112555d93dd44a280287b94e44981291eb
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-04-25 00:55:30 -0500

    added DB to .gitignore, set up user-model

---
+++ data/helpers/users-model.js
@@ -0,0 +1,31 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
+
+function find() {
+  return db("users").select("id", "username", "password");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+async function add(user) {
+  const [id] = await db("users")
+    .insert(user)
+    .returning("id");
+  console.log(`User #${id} added`);
+  return findById(id);
+}
+
+function findById(id) {
+  console.log(`Finding user with the id ${id}`);
+  return db("users")
+    .where({ id })
+    .first();
+}
