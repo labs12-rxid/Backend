6f7943226c2a8772887db4f891c8b8718cb92d28
workingjubilee <workingjubilee@gmail.com>
2019-04-26 11:01:34 -0700

    Fixes User Router tests

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -7,7 +7,7 @@ userRouter.get("/:id", async (req, res) => {
   try {
     const user = await Users.findById(id);
     if (user) {
-      res.status(200).json(user);
+      res.status(200).json({ user });
     } else {
       res
         .status(404)
