db153a09bfa995cd2fde48aee1e9af88d2d1218b
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-25 23:43:55 -0400

    Added truncate option to cleaner seed.

--- data/seeds/000-cleaner.js
+++ data/seeds/000-cleaner.js
@@ -1,5 +1,8 @@
 const cleaner = require("knex-cleaner");
 
 exports.seed = function(knex) {
-  return cleaner.clean(knex);
+  const options = {
+    mode: "truncate" // Valid options 'truncate', 'delete'
+  };
+  return cleaner.clean(knex, options);
 };
