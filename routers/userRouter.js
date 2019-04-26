f91151c1dfa27dcfc10f4c5a79fa9bc756a59a8b
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-25 20:42:11 -0400

    Refactored userRouter to async await, error message will now pop up when you try to fetch a user that doesn't exist.

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,20 +2,20 @@ const userRouter = require("express").Router();
 
 const Users = require("../data/helpers/users-model");
 
-userRouter.get("/:id", (req, res) => {
-  let { id } = req.params;
-
-  Users.findById(id)
-    .then(user => {
-      res.status(201).json({ user });
-    })
-    .catch(error => {
-      console.error("Error retreiving user: ", error);
-      res.status(500).json({
-        error: error,
-        message: "Make sure user with ID exists"
-      });
-    });
+userRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await Users.findById(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json({ message: "User with specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: `User request failed ${error}.` });
+  }
 });
 
 module.exports = userRouter;
