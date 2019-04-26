850ce1ecf0f826f300805e7eb67e93436ce775f4
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-26 00:11:51 -0400

    Commented out user generator. Allowed prettier to format fakeUsers.

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,28 +1,28 @@
 const fs = require("fs");
 const faker = require("faker");
 const bcrypt = require("bcryptjs");
-// const fakeUsers = require("../dummyData/fakeUsers.json")["users"];
+const fakeUsers = require("../dummyData/fakeUsers.json")["users"];
 
-const createFakeUser = () => ({
-  email: faker.internet.email(),
-  first_name: faker.name.firstName(),
-  last_name: faker.name.lastName(),
-  username: faker.internet.userName(),
-  password: faker.internet.password(),
-  phone: faker.phone.phoneNumber()
-});
+// const createFakeUser = () => ({
+//   email: faker.internet.email(),
+//   first_name: faker.name.firstName(),
+//   last_name: faker.name.lastName(),
+//   username: faker.internet.userName(),
+//   password: faker.internet.password(),
+//   phone: faker.phone.phoneNumber()
+// });
 
 exports.seed = function(knex, Promise) {
-  const fakeUsers = [];
+  // const fakeUsers = [];
 
-  for (let i = 0; i < 500; i++) {
-    fakeUsers.push(createFakeUser());
-  }
+  // for (let i = 0; i < 500; i++) {
+  //   fakeUsers.push(createFakeUser());
+  // }
 
-  fs.writeFileSync(
-    "./data/dummyData/fakeUsers.json",
-    JSON.stringify({ users: fakeUsers })
-  );
+  // fs.writeFileSync(
+  //   "./data/dummyData/fakeUsers.json",
+  //   JSON.stringify({ users: fakeUsers })
+  // );
 
   const hashedFakeUsers = fakeUsers.map(user => {
     user.password = bcrypt.hashSync(user.password, 8);
