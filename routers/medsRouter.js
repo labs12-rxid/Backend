14e6ce38797cb705a5c0ef669cc583771006be5b
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-29 15:23:33 -0400

    Added routers for adding new med, deleting a med, or editing a med.

--- routers/medsRouter.js
+++ routers/medsRouter.js
@@ -4,7 +4,7 @@ const Meds = require("../data/helpers/meds-model");
 
 medsRouter.get("/", async (req, res) => {
   try {
-    const meds = await Meds.findMeds();
+    const meds = await Meds.find();
     res.status(200).json(meds);
   } catch (error) {
     res.status(500).json(error);
@@ -12,18 +12,76 @@ medsRouter.get("/", async (req, res) => {
 });
 
 medsRouter.get("/:id", async (req, res) => {
-  const id = req.params.id;
+  const { id } = req.params;
   try {
-    const med = await Meds.findMedById(id);
+    const med = await Meds.findById(id);
     if (med) {
       res.status(200).json(med);
     } else {
       res
         .status(404)
-        .json({ message: "User with specified ID does not exist." });
+        .json({ message: "Med with specified ID does not exist." });
     }
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+medsRouter.post("/", async (req, res) => {
+  const { user_id } = req.body;
+  if (!user_id) {
+    // We can add other necessary fields later.
+    res.status(401).json({ message: "No user ID submitted." });
+  } else {
+    try {
+      const newMed = await Meds.add(req.body);
+      if (newMed) {
+        res.status(201).json(newMed);
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `Your medication could not be posted ${error}.` });
+    }
+  }
+});
+
+medsRouter.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const review = await Meds.remove(id);
+    if (review) {
+      res.status(200).json(review);
+    } else {
+      res
+        .status(404)
+        .json({
+          message: "The medication with the specified ID does not exist."
+        });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: `The medication's information could not be modified: ${error}.`
+    });
+  }
+});
+
+medsRouter.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const med = req.body;
+  try {
+    const editedMed = await Meds.update(id, med);
+    if (editedMed) {
+      res.status(200).json(editedMed);
+    } else {
+      res.status(404).json({
+        message: "The medication with the specified ID does not exist."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: `The medication's information could not be modified: ${error}.`
+    });
   }
 });
 
