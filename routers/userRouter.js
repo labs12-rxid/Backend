b61962b25a1514f1c9fb5936170801c7469750e0
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-29 18:42:47 -0400

    Route for fetching a user's meds added.

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,7 @@
 const userRouter = require('express').Router();
 
 const Users = require('../data/helpers/users-model');
+const Meds = require('../data/helpers/meds-model');
 const Diary = require('../data/helpers/diary-model');
 
 userRouter.get('/:id', async (req, res) => {
@@ -19,6 +20,24 @@ userRouter.get('/:id', async (req, res) => {
   }
 });
 
+userRouter.get('/:id/meds', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const meds = await Meds.findUsersMeds(id);
+    if (meds) {
+      res.status(200).json(meds);
+    } else {
+      res.status(404).json({
+        message: 'User with specified ID does not have any medications.'
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `User medications request failed ${error}.` });
+  }
+});
+
 userRouter.get('/:id/diaries', async (req, res) => {
   const { id } = req.params;
   try {
@@ -28,7 +47,7 @@ userRouter.get('/:id/diaries', async (req, res) => {
     } else {
       res
         .status(404)
-        .json({ message: 'User with specified ID does not exist.' });
+        .json({ message: 'User with specified ID does not have any diaries.' });
     }
   } catch (error) {
     res.status(500).json({ message: `User diaries request failed ${error}.` });
