7563eed67d9c252d57ade3f9a57bf8d07be132a3
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-30 18:36:04 -0400

    Reorganized authRouter, changed .then syntax to async await syntax.

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,78 +1,61 @@
-const authRouter = require("express").Router();
-const bcrypt = require("bcryptjs");
+const authRouter = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Users = require('../data/helpers/users-model');
 
-const Users = require("../data/helpers/users-model");
+authRouter.post('/register', async (req, res) => {
+  let user = req.body;
+
+  if (!user.username || !user.password) {
+    res.status(401).json({ message: 'Please provide username and password.' });
+  } else {
+    user.password = bcrypt.hashSync(user.password, 8);
+    const newUser = await Users.add(user);
+    if (newUser) {
+      const token = generateUserToken(newUser);
+      res
+        .status(201)
+        .json({ message: `Welcome ${user.username}!`, token, id: newUser.id });
+    } else {
+      res.status(500).json({ message: `Error adding user ${error}.` });
+    }
+  }
+});
+
+authRouter.post('/login', async (req, res) => {
+  let { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(401).json({ message: 'Please provide username and password.' });
+  } else {
+    const user = await Users.findBy({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateUserToken(user);
+      res
+        .status(200)
+        .json({ message: `Welcome ${user.username}!`, token, id: user.id });
+    }
+  }
+});
 
 // Maybe pull this jwt code to a new folder?
-const jwt = require("jsonwebtoken");
-
-module.exports = {
-  generateUserToken
-};
 
 function generateUserToken(user) {
   const payload = {
     subject: user.id,
     username: user.username
   };
-  const secret = process.env.SECRET || "poopdiddyscoop";
+  const secret = process.env.SECRET || 'poopdiddyscoop';
   const options = {
-    expiresIn: "1h"
+    expiresIn: '7d'
   };
 
   return jwt.sign(payload, secret, options);
 }
 
 // End of JWT generation code
-
-authRouter.post("/register", (req, res) => {
-  let user = req.body;
-
-  if (!user.username || !user.password) {
-    res.status(400).json({ error: "Please provide username and password." });
-  }
-
-  user.password = bcrypt.hashSync(user.password, 8);
-
-  Users.add(user)
-    .then(added => {
-      console.log("Added user ", added);
-      const token = generateUserToken(user);
-      res.status(201).json({ token, id: added.id });
-    })
-    .catch(error => {
-      console.error("Error adding user: ", error);
-      res.status(500).json({
-        error: error,
-        message: "Username is possibly already taken"
-      });
-    });
-});
-
-authRouter.post("/login", (req, res) => {
-  let { username, password } = req.body;
-
-  if (!username || !password) {
-    res.status(400).json({ error: "Please provide username and password." });
-  }
-
-  Users.findBy({ username })
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateUserToken(user);
-
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-          token,
-          id: user.id
-        });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+module.exports = {
+  generateUserToken
+};
 
 module.exports = authRouter;
