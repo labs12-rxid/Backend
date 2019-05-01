dd39a4d6aef8c95485d21a398098906ae37b0403
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-05-01 16:45:06 -0400

    Updated diary-model to pull med_name along with diary entries.

--- data/helpers/diary-model.js
+++ data/helpers/diary-model.js
@@ -19,8 +19,25 @@ function findById(id) {
     .first();
 }
 
-function findBy(filter) {
-  return db('diaries').where(filter);
+const getUserValues = async user_id => {
+  return await db('user-values')
+    .leftJoin('default-values', 'default-values.id', 'default_value_id')
+    .leftJoin('created-values', 'created-values.id', 'created_value_id')
+    .select(
+      'user-values.id as id',
+      'created_value_name',
+      'default_value_name',
+      'value_rank',
+      'value_importance'
+    )
+    .where({ 'user-values.user_id': user_id });
+};
+
+function findBy(user_id) {
+  return db('diaries')
+    .leftJoin('meds', 'meds.id', 'med_id')
+    .select('diaries.id', 'med_name', 'diary_date', 'diary_emoji', 'diary_text')
+    .where({ 'diaries.user_id': user_id });
 }
 
 function add(diary) {
