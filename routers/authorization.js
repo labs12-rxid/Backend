f0b179dca43db0d38186a5b00a479b39edf23399
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-01 20:39:28 -0400

    Auth middleware will check for presence of token before running jwt.verify.

--- routers/authorization.js
+++ routers/authorization.js
@@ -2,28 +2,32 @@ const jwt = require('jsonwebtoken');
 const jwksRsa = require('jwks-rsa');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
-  const client = jwksRsa({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: process.env.JWKSURI
-  });
-  const options = {
-    algorithm: 'RS256'
-  };
-  function getKey(header, callback) {
-    client.getSigningKey(header.kid, (err, key) => {
-      let signingKey = key.publicKey || key.rsaPublicKey;
-      callback(null, signingKey);
+  if (req.headers.authorization) {
+    const token = req.headers.authorization;
+    const client = jwksRsa({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: process.env.JWKSURI
     });
-  }
-  jwt.verify(token, getKey, options, (error, decoded) => {
-    if (error) {
-      res.status(400).json({ message: 'Invalid token.' });
-    } else {
-      req.headers.decoded = decoded;
-      next();
+    const options = {
+      algorithm: 'RS256'
+    };
+    function getKey(header, callback) {
+      client.getSigningKey(header.kid, (err, key) => {
+        let signingKey = key.publicKey || key.rsaPublicKey;
+        callback(null, signingKey);
+      });
     }
-  });
+    jwt.verify(token, getKey, options, (error, decoded) => {
+      if (error) {
+        res.status(401).json({ message: 'Invalid token.' });
+      } else {
+        req.headers.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'No token provided. Access denied.' });
+  }
 };
