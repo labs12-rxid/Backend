9412f9a499f17c5ed0c1eade69fb58369b697358
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-05-06 18:56:26 -0400

    Added scheduler to index.js so the scheduler will start when the server is started.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
-const server = require('./api/server.js');
 
+const scheduler = require('./reminders/scheduler.js');
+
+const server = require('./api/server.js');
 const port = process.env.PORT || 5001;
 
 server.listen(port, function() {
