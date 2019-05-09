a27af1c8602bdfb1417f5c36efc1ea29aa196915
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-09 18:04:31 -0400

    Changing rem_date again..

--- data/migrations/20190424183217_rems.js
+++ data/migrations/20190424183217_rems.js
@@ -22,7 +22,7 @@ exports.up = function(knex) {
       .notNullable()
       .defaultTo('admin');
     table.string('rem_notes', 16);
-    table.integer('rem_date', 50);
+    table.float('rem_date', 50);
   });
 };
 
