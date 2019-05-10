4988cb213fa2aeefe230dc2062e58238066ef967
workingjubilee <workingjubilee@gmail.com>
2019-05-10 11:54:00 -0700

    Fixes upload to work

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -1,18 +1,21 @@
-const upload = require ('../api/upload.js'); // Brings in multer.
+const upload = require('../api/upload.js')[upload]; // Brings in multer.
+const filepath = require('../api/upload.js')[uploadTo]; // Brings in multer.
 const router = require('express').Router();
 const axios = require('axios');
+const path = require('path');
 
-// router.post('/', upload.single('image'), async (req, res) => {
-router.get('/', async (req, res) => {
-  // const file = req.file;
-  // console.log(req.body);
-  // console.log(req.file);
+router.post('/', upload.single('image'), async (req, res) => {
+  const file = req.file;
+  console.log(req.body);
+  console.log(req.file);
 
-  // if (!file) {
-  //   res.status(400).json({ message: "No file provided." })
-  // } else {
+  if (!file) {
+    res.status(400).json({ message: "No file provided." })
+  } else {
     try {
-      const dataMagic = await axios.post("http://localhost:8000/rekog", { "wham": "bam" })
+      const dataMagic = await axios.post("https://rxid-ds.us-east-2.elasticbeanstalk.com/", {
+        "file_locations": [`d2ccuggcw9y16c.cloudfront.net/api/upload/${file.filename}`]
+      };
         // body: {
         //   image_file: `file://${file.path}`
         // }
@@ -22,6 +25,16 @@ router.get('/', async (req, res) => {
       res.status(500).json({ message: "Internal server error." });
     }
   // }
-});
+};
+
+router.get('/:filename', async (req, res) => {
+  const { filename } = req.params;
+  let fileLocation = `${filepath}/${filename}`;
+  try {
+    res.status(200).sendFile(fileLocation)
+  } catch(error) {
+    res.status(500).json({ message: "Internal server error." })
+  }
+})
 
 module.exports = router;
