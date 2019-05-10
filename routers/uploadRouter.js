814dda01a9cadbd7c11a004a622870ac4e552fbe
workingjubilee <workingjubilee@gmail.com>
2019-05-10 14:48:14 -0700

    Fixing upload, tightening buildspec to prevent negligent CIs.

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -1,5 +1,5 @@
-const upload = require('../api/upload.js')[upload]; // Brings in multer.
-const filepath = require('../api/upload.js')[uploadTo]; // Brings in multer.
+const upload = require('../api/upload.js')['upload']; // Brings in multer.
+const filepath = require('../api/upload.js')['uploadTo']; // Brings in multer.
 const router = require('express').Router();
 const axios = require('axios');
 const path = require('path');
@@ -9,27 +9,27 @@ router.post('/', upload.single('image'), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
 
+  const here = process.env.MY_CRIB;
+
   if (!file) {
     res.status(400).json({ message: "No file provided." })
   } else {
     try {
-      const dataMagic = await axios.post("https://rxid-ds.us-east-2.elasticbeanstalk.com/", {
-        "file_locations": [`d2ccuggcw9y16c.cloudfront.net/api/upload/${file.filename}`]
-      };
-        // body: {
-        //   image_file: `file://${file.path}`
-        // }
-      // });
+      const dataMagic = await axios.post(`${}`, {
+        "file_locations": [`${here}/api/upload/${file.filename}`]
+      });
+
       res.status(200).json({ ...dataMagic.data });
     } catch (error) {
       res.status(500).json({ message: "Internal server error." });
     }
   // }
-};
+  }
 
 router.get('/:filename', async (req, res) => {
   const { filename } = req.params;
   let fileLocation = `${filepath}/${filename}`;
+  
   try {
     res.status(200).sendFile(fileLocation)
   } catch(error) {
