a685b6e0ef77c9c27c016d99858f2187bf7175e1
workingjubilee <workingjubilee@gmail.com>
2019-05-09 18:15:48 -0700

    Experimental router update

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 
 router.post('/', upload.single('image'), function (req, res) {
   const file = req.file;
+  console.log(req.body);
 
   if (!file) {
     res.status(400).json({ message: "No file provided." })
   } else {
     try {
-      res.status(200).send(file);
-      // Will use a slightly different syntax out of dev, this exposes server path names.
+      res.status(303).redirect('http://localhost:8000');
     } catch (error) {
       res.status(500).json({ message: "Internal server error." });
     }
