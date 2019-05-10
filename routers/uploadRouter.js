36b31cf5eef06f50f1b49dfbb03a1416cd78bd99
workingjubilee <workingjubilee@gmail.com>
2019-05-10 02:58:26 -0700

    Functional image router prototype
    Commented code contains most of what is missing on our end.
    Requires update of the Flask code to work.

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -1,19 +1,27 @@
 const upload = require ('../api/upload.js'); // Brings in multer.
 const router = require('express').Router();
+const axios = require('axios');
 
-router.post('/', upload.single('image'), function (req, res) {
-  const file = req.file;
-  console.log(req.body);
+// router.post('/', upload.single('image'), async (req, res) => {
+router.get('/', async (req, res) => {
+  // const file = req.file;
+  // console.log(req.body);
+  // console.log(req.file);
 
-  if (!file) {
-    res.status(400).json({ message: "No file provided." })
-  } else {
+  // if (!file) {
+  //   res.status(400).json({ message: "No file provided." })
+  // } else {
     try {
-      res.status(303).redirect('http://localhost:8000');
+      const dataMagic = await axios.post("http://localhost:8000/rekog", { "wham": "bam" })
+        // body: {
+        //   image_file: `file://${file.path}`
+        // }
+      // });
+      res.status(200).json({ ...dataMagic.data });
     } catch (error) {
       res.status(500).json({ message: "Internal server error." });
     }
-  }
+  // }
 });
 
 module.exports = router;
