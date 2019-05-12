d70d8481212c912d4e4a2fee312def97d98ebd42
workingjubilee <workingjubilee@gmail.com>
2019-05-12 01:51:32 -0700

    Needed to fix the image uploading

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -3,25 +3,32 @@ const filepath = require('../api/upload.js')['uploadTo']; // Brings in multer.
 const router = require('express').Router();
 const axios = require('axios');
 const path = require('path');
+const util = require('util');
+require('dotenv').config(); 
 
 router.post('/', upload.single('image'), async (req, res) => {
   const file = req.file;
-  console.log(req.body);
-  console.log(req.file);
-
-  const here = process.env.MY_CRIB || localhost;
-  const science = process.env.DS_SERVER || localhost;
+  const here = process.env.MY_CRIB;
+  const science = process.env.DS_SERVER;
 
   if (!file) {
     res.status(400).json({ message: "No file provided." })
   } else {
     try {
-      const dataMagic = await axios.post(`${science}/rekog`, {
-        "file_locations": [`${here}/api/upload/${file.filename}`]
+      const imageLocArray = [`${here}/api/upload/${file.filename}`];
+      const rekogEndpoint = `${science}/rekog`;
+
+      const dataMagic = await axios({
+        url: rekogEndpoint, 
+        method: 'post',
+        data: {
+          "image_locations": imageLocArray
+        }
       });
 
-      res.status(200).json({ ...dataMagic.data });
+      res.status(200).json({ data: dataMagic.data });
     } catch (error) {
+      console.log(error);
       res.status(500).json({ message: "Internal server error." });
     }
   // }
@@ -32,8 +39,10 @@ router.get('/:filename', async (req, res) => {
   const { filename } = req.params;
   let fileLocation = `${filepath}/${filename}`;
 
+  console.log(fileLocation);
+
   try {
-    res.status(200).sendFile(fileLocation)
+    res.status(200).type('png').sendFile(fileLocation)
   } catch(error) {
     res.status(500).json({ message: "Internal server error." })
   }
